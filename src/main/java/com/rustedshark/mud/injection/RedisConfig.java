57b1ddb38702746fc06ef0a2ea6fbf87fb4a9fbf
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 00:38:59 -0500

    Minor tweaks to gradle

--- src/main/java/com/rustedshark/mud/injection/RedisConfig.java
+++ src/main/java/com/rustedshark/mud/injection/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisServer;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.embedded.RedisServerBuilder;
 
 import javax.annotation.PostConstruct;
@@ -11,12 +13,25 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class RedisConfig {
 
+    @Value("${mud.redis.host:localhost}")
+    private String _host;
+
     @Value("${mud.redis.port:6379}")
     private int _port;
 
     @Bean
-    public RedisServer redisServer() {
-        return new RedisServer("localhost", _port);
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(_host);
+        jedisConnectionFactory.setPort(_port);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 
 }
