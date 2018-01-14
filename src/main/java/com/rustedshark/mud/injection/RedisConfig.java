21cadf32a02805cec1d47aac5b27f7f8f006c53f
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 00:42:15 -0500

    Removing bad import

--- src/main/java/com/rustedshark/mud/injection/RedisConfig.java
+++ src/main/java/com/rustedshark/mud/injection/RedisConfig.java
@@ -3,12 +3,8 @@ package com.rustedshark.mud.injection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisServer;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.embedded.RedisServerBuilder;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 public class RedisConfig {
