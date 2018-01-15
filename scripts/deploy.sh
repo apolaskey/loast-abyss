e2062c4106ede8b2d459775261ca54ad04642b2c
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 23:38:36 -0500

    RTFM the Travis docs, TRAVIS_PULL_REQUEST is either false or the PR number

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,10 @@ gradle docker
 export PATH=$PATH:$HOME/.local/bin
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-if [[ ( $TRAVIS_BRANCH == "master" && ! $TRAVIS_PULL_REQUEST ) ]]; then
+if [[ ( $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ) ]]; then
   echo "Pushing build to production"
-elif [[ ( $TRAVIS_BRANCH == "latest" && ! $TRAVIS_PULL_REQUEST ) ]]; then
+  gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
+elif [[ ( $TRAVIS_BRANCH == "latest" && $TRAVIS_PULL_REQUEST == "false" ) ]]; then
   echo "Pushing build to latest"
 else
   echo "Skipping deploy, no defined environment"
