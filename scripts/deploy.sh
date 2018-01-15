972e14b339334e9b8cd2b4e4abdb124ad9723a2c
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 22:01:16 -0500

    Cleaning script up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,9 @@ gradle docker
 export PATH=$PATH:$HOME/.local/bin
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-if [[ $TRAVIS_BRANCH == "master" ]]
+if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Pushing build to production"
   gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
-if [[ $TRAVIS_BRANCH == "latest" ]]
+elif [[ $TRAVIS_BRANCH == "latest" ]]; then
   echo "Pushing build to latest"
 fi
