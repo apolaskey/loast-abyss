148da9bf2263159c7262af68c270ba464a78010c
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 20:36:37 -0500

    Travis update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   echo "Pushing build to production"
-  pip install --user awscli
-  export PATH=$PATH:$HOME/.local/bin
-  $(aws ecr get-login --no-include-email --region us-east-1)
-  gradle docker
-  gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
+elif [[ "$TRAVIS_BRANCH" == "latest" ]]; then
+  echo "Pushing build to latest"
 fi
