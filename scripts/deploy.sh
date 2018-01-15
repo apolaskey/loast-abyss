0afe28a151c9d07a213f9ab65ba479683077b619
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 22:28:17 -0500

    Adding PR detection

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,11 @@ gradle docker
 export PATH=$PATH:$HOME/.local/bin
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-echo $TRAVIS_BRANCH
+echo "$TRAVIS_PULL_REQUEST"
 
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
   echo "Pushing build to production"
-elif [[ $TRAVIS_BRANCH == "latest" ]]; then
+  gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
+elif [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
   echo "Pushing build to latest"
 fi
