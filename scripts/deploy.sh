f1cb75cc74dcb8649de6a5b0c6ab8f051afabad5
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 22:38:42 -0500

    Removing double quotes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ gradle docker
 export PATH=$PATH:$HOME/.local/bin
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-echo "$TRAVIS_PULL_REQUEST"
+echo $TRAVIS_PULL_REQUEST
 
-if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST = "false" ]]; then
   echo "Pushing build to production"
   gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
-elif [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+elif [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST = "true" ]]; then
   echo "Pushing build to latest"
 fi
