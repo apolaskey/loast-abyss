ed616379f6a9df1bb2db888bf0849474e736896b
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 20:56:07 -0500

    Attempt 2 at build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[$TRAVIS_BRANCH == "master" ]]; then
   echo "Pushing build to production"
-elif [[ "$TRAVIS_BRANCH" == "latest" ]]; then
+  export PATH=$PATH:$HOME/.local/bin
+  $(aws ecr get-login --no-include-email --region us-east-1)
+  gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
+elif [[ $TRAVIS_BRANCH == "latest" ]]; then
   echo "Pushing build to latest"
 fi
