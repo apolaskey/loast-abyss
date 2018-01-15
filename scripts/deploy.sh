b1da6ea776de409ba9d1efd975e0d5ffc669876d
Andrew Polaskey <apolaskey@gmail.com>
2018-01-14 23:07:50 -0500

    Updating with shell checker suggestions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo "Branch: $TRAVIS_BRANCH isPushRequest (0 == false, 1 == true): $TRAVIS_PULL_REQUEST"
+echo "Branch: $TRAVIS_BRANCH isPushRequest: $TRAVIS_PULL_REQUEST"
 
 pip install --user awscli
 gradle docker
 export PATH=$PATH:$HOME/.local/bin
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-if [[ ( $TRAVIS_BRANCH == "master" && !$TRAVIS_PULL_REQUEST ) ]]; then
+if [[ ( $TRAVIS_BRANCH == "master" && ! $TRAVIS_PULL_REQUEST ) ]]; then
   echo "Pushing build to production"
-  gradle dockerPush -Ddocker.repo=974758436222.dkr.ecr.us-east-1.amazonaws.com
-elif [[ ( $TRAVIS_BRANCH == "latest" && !$TRAVIS_PULL_REQUEST ) ]]; then
+elif [[ ( $TRAVIS_BRANCH == "latest" && ! $TRAVIS_PULL_REQUEST ) ]]; then
   echo "Pushing build to latest"
 else
   echo "Skipping deploy, no defined environment"
